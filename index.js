76e0b8fdae9254b6c1fd528e97f7bc1b1b40a553
Micheni58 <michenicaleb@gmail.com>
2025-06-17 19:13:54 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+// function submitData(userName,userEmail){
+//    fetch("http://localhost:3000/users",{
+//     method:'POST',
+//     headers:{
+//         'Content-Type': 'application/json',
+//         'Accept':'application/json'
+//     },
+//     body:JSON.stringify({
+//         name:userName,
+//         email:userEmail
+//     })
+//    }) 
+//    .then((res) => res.json())
+//    .then(data =>{
+//     document.body.innerHTML += `<p>${data.id}</p>`;
+//    })
+// }
+
+  function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.body.innerHTML += `<p>${data.id}</p>`;
+  })
+  .catch(error => {
+    document.body.innerHTML += `<p>${error.message}</p>`;
+  });
+}
